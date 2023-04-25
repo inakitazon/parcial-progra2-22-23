7ac50f73ecbd446c018add5b0c8d0e859a894b42
inakitazon <98150286+inakitazon@users.noreply.github.com>
2023-04-25 13:16:40 +0200

    Add files via upload

---
+++ entregas/Iñakitazón/examen-parcial-p2/src/main/ex1/Person.java
@@ -0,0 +1,14 @@
+package main.ex1;
+
+public class Person {
+	private final String name;
+	private final String surname;´
+	private final String address;
+	
+	public Person(String john, String doe, String s) {
+		this.name = john;
+		this,surname = doe;
+		this,address = s;
+	}
+
+}
