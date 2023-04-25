96f14b694ecd590217fad611dde1894664dfcc57
inakitazon <98150286+inakitazon@users.noreply.github.com>
2023-04-25 13:04:25 +0200

    Add files via upload

--- entregas/examen-parcial-p2/src/main/ex1/ManagementSystem.java
+++ entregas/examen-parcial-p2/src/main/ex1/ManagementSystem.java
@@ -1,38 +1,44 @@
-package main.ex1;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class ManagementSystem {
-    private FileHandler fileHandler;
-
-
-
-    public ManagementSystem() {
-    }
-
-    public void loadData(String path) {
-        /*
-            Load clients from file
-
-            // For each line in file
-
-            String[] data = line.split(";");
-
-             // Extract data from line and remove quotes
-            String name = data[0].replaceAll("\"", "");
-            String surname = data[1].replaceAll("\"", "");
-            String address = data[2].replaceAll("\"", "");
-            String phoneNumber = data[3].replaceAll("\"", "");
-
-
-            // Create client and add to list
-
-         */
-    }
-    public void addAppointment(Appointment appointment, Client client) {
-       // Add appointment to list
-    }
-
-}
+	
+	List<Appointment> appointments;
+	List<Person> personas;
+	List<Pet> mascotas,
+	
+	public veterinaryClinic() {
+		
+		this.appointments = new ArrayList<>();
+		this.personas = new Arraylist<>();
+		this.mascotas = new Arraylist<>();
+	}
+	
+	public void addAppointment(Appointment appointment) {
+		appointments.add(appointment);
+	}
+	
+	pulic void listAppointments() {
+		for (Appointment appointment : appointments) {
+			//Date - Pet - Reason
+			System.out.println(appointment.getDate()+ " - " + appointment.getPet() + " - " + appointment.getReason());
+			}
+	}
+	
+	
+	public String muestraFechas() {
+		
+		ArrayList<Date> auxiliar = new ArrayList<>();
+		
+		
+	}
+	
+	public Pet buscaMascota(Person p) {
+		for(Pet mascota: mascotas) {
+			if(mascota.getBueno().equals(p)) {
+				return mascota
+			}
+			return null;
+		}
+	}
+	}
+	public boolean añade Mascota()
